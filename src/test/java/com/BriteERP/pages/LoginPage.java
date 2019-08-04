cc1f628a9e0703866293b6ae8457d6eb764aae01
Sung Hun Baek <nicebsh93@gmail.com>
2019-08-04 19:21:09 -0400

    sung

--- src/test/java/com/BriteERP/pages/LoginPage.java
+++ src/test/java/com/BriteERP/pages/LoginPage.java
@@ -1,4 +1,37 @@
 package com.BriteERP.pages;
 
+import com.BriteERP.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(id="login")
+    public WebElement username;
+
+    @FindBy(id="password")
+    public WebElement password;
+
+    @FindBy (xpath = "//*[@id=\"wrapwrap\"]/main/div/form/div[3]/button")
+    public WebElement submit;
+
+
+    public void login(String usernameStr, String passwordStr){
+        username.sendKeys(usernameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+
+        WebDriverWait wait = new WebDriverWait(Driver.get(), 5);
+        wait.until(ExpectedConditions.titleIs("Dashboard"));
+
+    }
+
+
 }
