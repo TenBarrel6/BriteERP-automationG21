6cab951a6e8606b524814936232a8a80edc4dc54
Roman <slusarchuk.roman@gmail.com>
2019-08-13 18:27:49 -0400

    Hooks update dryRun

--- src/test/java/com/BriteERP/runners/TestRunner.java
+++ src/test/java/com/BriteERP/runners/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/com/BriteERP/features/",
-        glue = "com/BriteERP/step_definitions"
+        glue = "com/BriteERP/step_definitions",
+		dryRun = false
 )
 public class TestRunner {
 }
